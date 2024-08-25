34370dfc5fe05d9d2116c32d8d011203704531f9
Monty Harding <monty.harding@gmail.com>
2024-08-25 14:02:45 +0800

    mouse disabled by default

--- init.lua
+++ init.lua
@@ -105,7 +105,8 @@ vim.opt.number = true
 -- vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
+-- vim.opt.mouse = 'a'
+vim.opt.mouse = ''
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
