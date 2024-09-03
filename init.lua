c0031385e58017fb1152f0d2800add0202824444
Monty Harding <monty.harding@gmail.com>
2024-09-04 06:55:07 +0800

    fixed markdown-preview install/config

--- init.lua
+++ init.lua
@@ -271,6 +271,7 @@ require('lazy').setup({
     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
     ft = { 'markdown' },
     build = function()
+      vim.cmd [[Lazy load markdown-preview.nvim]]
       vim.fn['mkdp#util#install']()
     end,
   },
