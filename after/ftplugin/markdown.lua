be3a19b7388ac03ee4b22e07b388269575db283b
Monty Harding <monty.harding@gmail.com>
2024-11-11 18:51:15 +0800

    added changes to specific highlights, commented out

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,19 +1,29 @@
 -- Run this file when a markdown file is opened
--- -- Define custom highlight groups for each markdown heading level
--- vim.api.nvim_set_hl(0, '@markdown.heading.1', { fg = '#7aa89f', bold = true })
--- vim.api.nvim_set_hl(0, '@markdown.heading.2', { fg = '#ffc894', bold = true })
--- vim.api.nvim_set_hl(0, '@markdown.heading.3', { fg = '#ffa066', bold = true })
--- vim.api.nvim_set_hl(0, '@markdown.heading.4', { fg = '#e46876', bold = true })
--- vim.api.nvim_set_hl(0, '@markdown.heading.5', { fg = '#938aa9', bold = true })
--- vim.api.nvim_set_hl(0, '@markdown.heading.6', { fg = '#975fb8', bold = true })
---
--- -- Link the custom highlight groups to Treesitter's markdown heading captures
--- vim.api.nvim_set_hl(0, '@markup.heading.1.markdown', { link = '@markdown.heading.1' })
--- vim.api.nvim_set_hl(0, '@markup.heading.2.markdown', { link = '@markdown.heading.2' })
--- vim.api.nvim_set_hl(0, '@markup.heading.3.markdown', { link = '@markdown.heading.3' })
--- vim.api.nvim_set_hl(0, '@markup.heading.4.markdown', { link = '@markdown.heading.4' })
--- vim.api.nvim_set_hl(0, '@markup.heading.5.markdown', { link = '@markdown.heading.5' })
--- vim.api.nvim_set_hl(0, '@markup.heading.6.markdown', { link = '@markdown.heading.6' })
+-- Define custom highlight groups for each markdown heading level
+vim.api.nvim_set_hl(0, '@markdown.heading.1', { fg = '#7aa89f', bold = true })
+vim.api.nvim_set_hl(0, '@markdown.heading.2', { fg = '#ffc894', bold = true })
+vim.api.nvim_set_hl(0, '@markdown.heading.3', { fg = '#ffa066', bold = true })
+vim.api.nvim_set_hl(0, '@markdown.heading.4', { fg = '#e46876', bold = true })
+vim.api.nvim_set_hl(0, '@markdown.heading.5', { fg = '#938aa9', bold = true })
+vim.api.nvim_set_hl(0, '@markdown.heading.6', { fg = '#975fb8', bold = true })
+
+-- Link the custom highlight groups to Treesitter's markdown heading captures
+vim.api.nvim_set_hl(0, '@markup.heading.1.markdown', { link = '@markdown.heading.1' })
+vim.api.nvim_set_hl(0, '@markup.heading.2.markdown', { link = '@markdown.heading.2' })
+vim.api.nvim_set_hl(0, '@markup.heading.3.markdown', { link = '@markdown.heading.3' })
+vim.api.nvim_set_hl(0, '@markup.heading.4.markdown', { link = '@markdown.heading.4' })
+vim.api.nvim_set_hl(0, '@markup.heading.5.markdown', { link = '@markdown.heading.5' })
+vim.api.nvim_set_hl(0, '@markup.heading.6.markdown', { link = '@markdown.heading.6' })
+
+-- Links [Label](URL)
+-- Brackets
+-- vim.api.nvim_set_hl(0, '@markup.link.markdown_inline', { fg = '#00ff00', bold = false, bg = ''})
+
+-- Label
+-- vim.api.nvim_set_hl(0, '@markup.link.label.markdown_inline', { fg = '', bold = true, bg = ''})
+
+-- URL
+-- vim.api.nvim_set_hl(0, '@markup.link.url.markdown_inline', { fg = '#ff00ff', bold = false, bg = ''})
 
 -- Define custom highlight group for inline code
 -- vim.api.nvim_set_hl(0, '@markdown.markdown_inline', { fg = '#975fb8' })
