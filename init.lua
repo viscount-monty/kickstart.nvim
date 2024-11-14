8ced7e542c4f117b1f47ef79dc83067f9306379e
Monty Harding <monty.harding@gmail.com>
2024-11-14 09:01:30 +0800

    split treesitter language installs into multiple lines, added mermaid

--- init.lua
+++ init.lua
@@ -1065,7 +1065,20 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'mermaid',
+        'query',
+        'vim',
+        'vimdoc'
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
